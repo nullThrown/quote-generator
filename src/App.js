adb300932cb899ebdebd394966e09f4aec6aaf12
Weston Peters <westonrpr@yahoo.com>
2021-09-05 16:30:39 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { quotes } from './quotes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='main-container'>
+      <Header />
+      <QuoteGenerator />
+    </main>
   );
 }
 
 export default App;
+
+const Header = () => {
+  return (
+    <div className='header-box'>
+      <h1 className='logo'>Logo</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href='/'>Home</a>
+          </li>
+          <li>
+            <a href='/'>About</a>
+          </li>
+          <li>
+            <a href='/'>Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+const generateQuote = () => {
+  return Math.floor(Math.random() * quotes.length);
+};
+const QuoteGenerator = () => {
+  const [quote, setQuote] = useState(quotes[generateQuote()]);
+
+  const clickHandler = () => {
+    setQuote(quotes[generateQuote()]);
+  };
+
+  return (
+    <div className='quote-generator'>
+      <h2 className='title'>Quote Generator</h2>
+      <Quote quote={quote} />
+      <button className='btn' onClick={clickHandler}>
+        Change quote
+      </button>
+    </div>
+  );
+};
+
+const Quote = (props) => {
+  const { text, author } = props.quote;
+  return (
+    <>
+      <p className='quote'>{text}</p>
+      <p className='author'>-{author}</p>
+    </>
+  );
+};
---
+++ src/quotes.js
@@ -0,0 +1,42 @@
+export const quotes = [
+  {
+    text: 'The only people who never fail are those who never try.',
+    author: 'Ilka Chase',
+  },
+  {
+    text: 'Failure is just another way to learn how to do something right.',
+    author: 'Marian Wright Edelman',
+  },
+  {
+    text: 'I failed my way to success.',
+    author: 'Thomas Edison',
+  },
+  {
+    text: 'Every failure brings with it the seed of an equivalent success.',
+    author: 'Napoleon Hill',
+  },
+  {
+    text: 'Only those who dare to fail greatly can ever achieve greatly.',
+    author: 'John F. Kennedy',
+  },
+  {
+    text: 'It is hard to fail, but it is worse never to have tried to succeed.',
+    author: 'Theodore Roosevelt',
+  },
+  {
+    text: 'Imagination is more important than knowledge.',
+    author: 'Albert Einstein',
+  },
+  {
+    text: 'Hold fast to dreams, for if dreams die, life is a broken winged bird that cannot fly.',
+    author: 'Langston Hughes',
+  },
+  {
+    text: 'The future belongs to those who believe in the beauty of their dreams.',
+    author: 'Eleanor Roosevelt',
+  },
+  {
+    text: 'Go confidently in the direction of your dreams. Live the life you have imagined.',
+    author: 'Henry David Thoreau',
+  },
+];
